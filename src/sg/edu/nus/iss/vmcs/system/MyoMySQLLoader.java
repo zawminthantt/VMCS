2a72c0efde3adacf7ecbdb91b58bc26de87740ba
Myo Wai Yan Kyaw <myowaiyankyaw@hotmail.com>
2016-10-23 15:39:21 +0800

    Database Adapter

---
+++ src/sg/edu/nus/iss/vmcs/system/MyoMySQLLoader.java
@@ -0,0 +1,17 @@
+package sg.edu.nus.iss.vmcs.system;
+
+public class MyoMySQLLoader implements MyoAdvancedDataLoader {
+
+	@Override
+	public void loadMySQL(String fileName) {
+		// TODO Auto-generated method stub
+		System.out.println("Load MySQL");
+	}
+	
+	@Override
+	public void loadPostgreSQL(String fileName) {
+		// Do Nothing
+		
+	}
+
+}
---
+++ src/sg/edu/nus/iss/vmcs/system/MyoPostgreSQLLoader.java
@@ -0,0 +1,17 @@
+package sg.edu.nus.iss.vmcs.system;
+
+public class MyoPostgreSQLLoader implements MyoAdvancedDataLoader {
+
+	@Override
+	public void loadMySQL(String fileName) {
+		// Do Nothing
+
+	}
+
+	@Override
+	public void loadPostgreSQL(String fileName) {
+		// TODO Auto-generated method stub
+		System.out.println("PostgreSQL Loader");
+	}
+
+}
